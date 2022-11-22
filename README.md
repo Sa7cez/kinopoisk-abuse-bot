a405d0d03dd57d90d890232d2ace2ced21963fb9
Sa7cez <sa7cez@gmail.com>
2022-11-22 07:23:10 +0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # kinopoisk-abuse-bot
 Happy Birthday, Kinopoisk!
+
+## Requeremets
+<b>To run this bot you need to have cookies of yandex account. Get it from page https://www.kinopoisk.ru/special/birthday19/ via DevTools or Chrome extension</b>
+
+## Setup bot
+1) Download ZIP and extract it to a folder
+2) Install node.js: `https://nodejs.org/en/` (LTS)
+3) Open folder with the bot in `cmd`
+```bash
+cd <path to folder with script>
+```
+5) Install dependencies
+```bash
+npm install
+```
+6) Start
+```bash
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+const axios = require('axios')
+const delay = require('delay')
+const fs = require('fs/promises')
+const randomInt = (value) => Math.floor(Math.random() * value)
+
+let answers = {}
+    cookies = {}
+
+const TIMEOUT = 1000
+
+const instance = axios.create({
+  baseURL: 'https://kp-guess-game-api.kinopoisk.ru/v1',
+  headers: {
+    accept: 'application/json, text/plain, */*',
+    'accept-language': 'ru-RU,ru;q=0.5',
+    'content-type': 'application/json',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'sec-gpc': '1',
+    "referer": 'https://www.kinopoisk.ru/',
+    'referrer-policy': 'strict-origin-when-cross-origin'
+  }
+})
+
+const kinopoisk = async (account, cookie, gameId) => {
+  const targets = {
+    1: 175 + randomInt(100),
+    2: 100 + randomInt(50),
+    3: 175 + randomInt(100),
+    4: 150 + randomInt(50),
+    5: 100 + randomInt(50),
+    6: 100 + randomInt(50),
+  }
+  const headers = { headers : { cookie: cookie } }
+  const game = (await instance.post('games', { gameId: gameId }, headers)).data
+  let id = game.stateData.question.id
+      prediction = answers[gameId][id] || game.stateData.question.answers[randomInt(4)]
+      play = true
+      points = 0
+
+  while (play) {
+    await delay(randomInt(TIMEOUT))
+    await instance
+      .post('questions/answers', { answer: prediction }, headers)
+      .then(async (r) => {
+        const answer = r.data
+        if (answer.correctAnswer.length > 0) answers[gameId][parseInt(id)] = answer.correctAnswer
+        points = answer.stateData.points
+        console.log(`${gameId} | ${account} | очков:`, points, answer.correctAnswer)
+        
+        if (!answer.stateData.question)
+          play = false
+        else {
+          id = answer.stateData.question.id
+          prediction = answers[gameId][id] || answer.stateData.question.answers[randomInt(4)]
+        }
+
+        if (!r.data.isCorrect)
+          await fs.writeFile('answers.json', JSON.stringify(answers))
+      })
+      .catch(e => console.log(e))
+  }
+  if (points === answers[gameId].length || points >= targets[gameId])
+    return true
+  return kinopoisk(account, cookie, gameId)
+}
+
+const main = async () => {
+  try {
+    cookies = (await fs.readFile('cookies.txt', 'utf8')).split('\n')
+  } catch {
+    console.log('Создайте файл cookies.txt, где в каждой строчке ваши куки для Кинопоиска.')
+  }
+  const games = (await instance.get('https://kp-guess-game-api.kinopoisk.ru/v1/episodes')).data.episodes.filter(episode => new Date(episode.startsAt) > new Date())
+  answers = JSON.parse(await fs.readFile('answers.json', 'utf8'))
+  for (let i = 1; i <= games.length; i++) {
+    console.log(`В базе ${Object.keys(answers[i]).length} ответов на ${i} квиз!`)
+    await Promise.all(cookies.map((cookie, j) => kinopoisk(j, cookie, i).catch(e => { console.log(e) })))
+  }
+}
+
+main()
